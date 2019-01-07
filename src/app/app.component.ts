4682f71e11406d548080e400be44d0e3671d7047
Raul Martinez <martinezvalenzuelaraul@gmail.com>
2019-01-06 23:04:49 -0700

    listo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit  {
+  profileForm = new FormGroup({
+    name: new FormControl(''),
+    modality: new FormControl('Unidiciplinaria'),
+    experience :new FormControl(''),
+    career:new FormControl('Ingenieria Civil'),
+    adviser:new FormControl(''),
+    cycle:new FormControl(''),
+    vence:new FormControl('')
+
+		
+  });
+
+  constructor(private httpClient: Http) { }
+
+  ngOnInit() {
+      
+  }
+  
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    var data : any;
+    data = this.profileForm.value;
+    console.log(data);
+    return this.httpClient.post('https://serviceuas.herokuapp.com/',data);
+
+  }
+
+  
 }
